524071e161eba230d366ba53e286cb60e56496c5
Lucas Briz <lucasbriz@outlook.com>
2024-12-03 13:47:53 -0300

    project initialization

---
+++ src/main/java/com/lucorp/bookcase_manager/BookcaseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lucorp.bookcase_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcaseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcaseManagerApplication.class, args);
+	}
+
+}
